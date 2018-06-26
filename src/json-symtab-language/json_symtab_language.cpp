95af4f9210eebbf274266914e30a4abbb0588d1f
Daniel Poetzl <daniel.poetzl@diffblue.com>
2018-06-26 18:20:09 +0100

    Resolve type symbols in symbol table when loading from json

--- src/json-symtab-language/json_symtab_language.cpp
+++ src/json-symtab-language/json_symtab_language.cpp
@@ -11,6 +11,7 @@ Author: Chris Smowton, chris.smowton@diffblue.com
 #include "json_symtab_language.h"
 #include <json/json_parser.h>
 #include <util/json_symbol_table.h>
+#include <util/namespace.h>
 
 bool json_symtab_languaget::parse(
   std::istream &instream,
@@ -30,12 +31,58 @@ bool json_symtab_languaget::typecheck(
   try
   {
     symbol_table_from_json(parsed_json_file, symbol_table);
+    follow_type_symbols(symbol_table);
     return false;
   }
   catch(const std::string &str)
   {
     error() << "json_symtab_languaget::typecheck: " << str << eom;
     return true;
+  }
+}
+
+void json_symtab_languaget::follow_type_symbols(
+  irept &irep,
+  const namespacet &ns)
+{
+  ns.follow_type_symbol(irep);
+
+  for(irept &sub : irep.get_sub())
+  {
+    follow_type_symbols(sub, ns);
+  }
+
+  for(auto &entry : irep.get_named_sub())
+  {
+    irept &sub = entry.second;
+
+    follow_type_symbols(sub, ns);
+  }
+}
+
+void json_symtab_languaget::follow_type_symbols(symbol_tablet &symbol_table)
+{
+  const namespacet ns(symbol_table);
+
+  std::vector<irep_idt> type_symbol_names;
+
+  for(auto &entry : symbol_table)
+  {
+    symbolt &symbol = symbol_table.get_writeable_ref(entry.first);
+
+    if(symbol.is_type)
+    {
+      type_symbol_names.push_back(symbol.name);
+    }
+
+    // Modify entries in place
+    follow_type_symbols(symbol.type, ns);
+    follow_type_symbols(symbol.value, ns);
+  }
+
+  for(const irep_idt &id : type_symbol_names)
+  {
+    symbol_table.remove(id);
   }
 }
 
